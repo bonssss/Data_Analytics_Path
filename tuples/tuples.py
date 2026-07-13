feb9c304a999bfdc1b3ce2832abfb2cb316b73f4
Bons <bons6710hos@gmail.com>
2026-07-13 15:44:59 +0300

    add tuple operations for accessing and slicing elements

---
+++ tuples/tuples.py
@@ -0,0 +1,5 @@
+tpls=(1,4,5,6,7)
+print(tpls[0])  # Accessing the first element of the tuple
+print(tpls[-1])  # Accessing the last element of the tuple
+
+print (tpls[1:4])  # Slicing the tuple to get elements from index 1 to 3
