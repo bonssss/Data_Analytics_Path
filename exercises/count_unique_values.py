94daa592a9a62b35d6a4af13ee97ed7b228d65b8
Bons <bons6710hos@gmail.com>
2026-07-13 16:02:37 +0300

    add functions to find unique elements in a list and to reverse a string

---
+++ exercises/count_unique_values.py
@@ -0,0 +1,7 @@
+def find_unique_elements(lst):
+    # your code here
+    
+    return len(set(lst))
+    
+print(find_unique_elements([2,2,4,5]))
+
---
+++ exercises/reverse_string.py
@@ -0,0 +1,7 @@
+def reverse_string(input_string):
+    # Your code here
+    reverse=input_string[::-1]
+    
+    return  reverse
+
+print(reverse_string("Hello"))
